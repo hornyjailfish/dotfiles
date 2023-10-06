8e70d0c38e9f2ba2b3aab6e5d4a68e5e5743d8a2
unknown <insight.outside@disroot.org>
2023-10-06 17:43:55 +0300

    upd

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -8,6 +8,7 @@ return {
 			{ "<leader>gg", "<cmd>Neogit<cr>", desc = "Neogit" },
 			{ "<leader>gb", "<cmd>Neogit branch<cr>", desc = "Neogit branch" },
 			{ "<leader>gc", "<cmd>Neogit commit<cr>", desc = "Neogit commit" },
+			{ "<leader>gp", "<cmd>Neogit push<cr>", desc = "Neogit push" },
 		},
 		opts = {
 			disable_builtin_notifications = true,
