85b83a29fc0de72584423e162a80d11e120a8ac0
unknown <insight.outside@disroot.org>
2025-08-19 09:20:35 +0300

    win-picker hijack

--- nvim/lua/s/plugins/win-pick.lua
+++ nvim/lua/s/plugins/win-pick.lua
@@ -1,4 +1,5 @@
 local util = require "s.util"
+local custom_filter = require("s.window-picker.custom-window-filter")
 
 local selection_display = function(char, window)
 	local bufnr = vim.api.nvim_win_get_buf(window)
@@ -28,19 +29,17 @@ local selection_display = function(char, window)
 	end
 end
 
+
 return {
-	-- "s1n7ax/nvim-window-picker",
+	"s1n7ax/nvim-window-picker",
 	version = "v2.*",
-	dev = true,
-	name = "nvim-window-picker",
-	dir = "~/gits/nvim-window-picker/",
+	-- dev = false,
+	-- name = "nvim-window-picker",
+	-- dir = "~/gits/nvim-window-picker/",
 	keys = {
 		{
 			"<leader>W",
 			function()
-				local filters = util.filter.filters
-				filters.inverse = true
-				local list = vim.api.nvim_tabpage_list_wins(vim.api.nvim_get_current_tabpage())
 				local picked_window_id = require('window-picker').pick_window({
 					-- if you want to manually filter out the windows, pass in a function that
 					-- takes two parameters. You should return window ids that should be
@@ -51,7 +50,7 @@ return {
 					--    -- return only the ones you want to include
 					--    return {1000, 1001}
 					-- end
-					filter_rules = filters,
+					filter_rules = custom_filter.make_rules(true),
 					picker_config = {
 						statusline_winbar_picker = {
 							selection_display = selection_display
@@ -59,23 +58,16 @@ return {
 						},
 					},
 				}) or vim.api.nvim_get_current_win()
-				-- local picked_window_id = require("window-picker").pick_window({ other_win_hl_color = color })
-				-- or vim.api.nvim_get_current_win()
-				-- local bufnr = 2 -- Replace with the buffer number you want to check
-				-- local buftype = vim.api.nvim_buf_get_option(picked_window_id, 'buftype')
 				vim.api.nvim_set_current_win(picked_window_id)
-				-- end
 			end,
 			desc = "Pick a window",
 		},
+
 		{
 			"<leader>w",
 			function()
-				local filters = util.filter.filters
-				filters.inverse = false
-				local list = vim.api.nvim_tabpage_list_wins(vim.api.nvim_get_current_tabpage())
 				local picked_window_id = require('window-picker').pick_window({
-					filter_rules = filters,
+					filter_rules = custom_filter.make_rules(),
 					picker_config = {
 						statusline_winbar_picker = {
 							selection_display = selection_display
@@ -87,14 +79,17 @@ return {
 			desc = "Pick a window",
 		},
 	},
+
 	config = function()
 		local _, color = require("nvim-web-devicons").get_icon_color_by_filetype(vim.bo.filetype)
 		require("window-picker").setup({
 			autoselect_one = true,
+			include_unfocusabel_windows = false,
 			include_current = false,
 			-- TODO: use different tags for different layouts
 			selection_chars = util.get_tags_for_layout(vim.g.layout),
 			current_win_hl_color = color,
+			filter_func = custom_filter.filter_func
 		})
 	end,
 }
